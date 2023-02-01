2c58fb2d891a71c18a50cfce1ef789048f513be1
Tung111222 <100447136+Tung111222@users.noreply.github.com>
2023-02-01 12:24:49 +0700

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -1,7 +1,6 @@
-
 #!/bin/bash
 # bản chạy 443 và 80 xrayr thường
-bash <(curl -ls https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/xrayrthuong.sh)
+bash <(curl -ls https://raw.githubusercontent.com/Tung111222/xrayr/main/xrayr.sh)
 
 
     read -p "Nhập Link Web :" domain
