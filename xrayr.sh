05ec5ff8d7b35a45aaf364e06c8368caeab2198e
Tung111222 <100447136+Tung111222@users.noreply.github.com>
2023-02-01 12:37:54 +0700

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -1,129 +1,128 @@
-#!/bin/bash
-# bản chạy 443 và 80 xrayr thường
-bash <(curl -ls https://raw.githubusercontent.com/Tung111222/xrayr/main/xrayr.sh)
+apt-get update -y
+sudo apt update
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 80
+sudo ufw allow 443
+lam='\033[1;34m'        
+tim='\033[1;35m'
+bash <(curl -Ls https://raw.githubusercontent.com/Tung111222/xrayr/main/xrayr.sh)
+read -p " Địa chỉ web(VD: https://example.com): " api_host
+  [ -z "${api_host}" ] && api_host=https://example.com
 
+read -p " Khóa giao tiếp(VD: example_123): " api_key
+  [ -z "${api_key}" ] && api_key=example_123  
 
-    read -p "Nhập Link Web :" domain
-    echo -e "Link Web là : ${domain}"
-
-    read -p "Nhập Api Key :" APIKEY
-    echo -e "API KEY là : ${APIKEY}"
-
-    read -p "Nhập Node ID port 80 :" node_80
-    echo -e "Node_80 là : ${node_80}"
-
-    read -p "Nhập Node ID port 443 :" node_443
-    echo -e "Node_443 là : ${node_443}"    
-
-    read -p "Nhập SpeedLimit :" SpeedLimit
-    echo -e "SpeedLimit = ${SpeedLimit}"
-
-    read -p "Nhập DeviceLimit :" DeviceLimit
-    echo -e "DeviceLimit = ${DeviceLimit}"
-
-    read -p "Nhập dns trên cloudflare node 443:" CertDomain443
-    echo -e "CertDomain443 = ${CertDomain443}"           
+read -p " NODE ID Cổng 80: " node_id1
+  [ -z "${node_id1}" ] && node_id1=0
   
+read -p " NODE ID Cổng 443: " node_id2
+  [ -z "${node_id2}" ] && node_id2=0
+rm -rf /etc/XrayR/htpn.crt
+rm -rf /etc/XrayR/htpn.key
+openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes -out /etc/XrayR/ht4g.crt -keyout /etc/XrayR/ht4g.key -subj "/C=JP/ST=Tokyo/L=Chiyoda-ku/O=Google Trust Services LLC/CN=google.com"
 cd /etc/XrayR
-
-EOF
 cat >config.yml <<EOF
 Log:
-  Level: none # Log level: none, error, warning, info, debug 
+  Log:
+  Level: none 
   AccessPath: # /etc/XrayR/access.Log
   ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
-RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
-ConnetionConfig:
-  Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 10 # Connection idle time limit, Second
-  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
-  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
+DnsConfigPath: # /etc/XrayR/dns.json
+InboundConfigPath: # /etc/XrayR/custom_inbound.json
+RouteConfigPath: # /etc/XrayR/route.json
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json
+ConnectionConfig:
+  Handshake: 4 
+  ConnIdle: 30 
+  UplinkOnly: 2 
+  DownlinkOnly: 4 
+  BufferSize: 64 
 Nodes:
   -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    PanelType: "V2board" 
     ApiConfig:
-      ApiHost: "$domain"
-      ApiKey: "$APIKEY"
-      NodeID: $node_80
-      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: $SpeedLimit # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: $DeviceLimit # Local settings will replace remote settings, 0 means disable
-      RuleListPath: /etc/XrayR/AikoBlock # /etc/XrayR/rulelist Path to local rulelist file
+      ApiHost: "https://4g.tintuc247.top"
+      ApiKey: "azzphucfreeinternet"
+      NodeID1: 1
+      NodeType: V2ray 
+      Timeout: 30 
+      EnableVless: false 
+      EnableXTLS: false 
+      SpeedLimit: 0
+      DeviceLimit: 0
+      RuleListPath: # /etc/XrayR/rulelist
     ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
+      DisableSniffing: True
+      ListenIP: 0.0.0.0 
+      SendIP: 0.0.0.0 
+      UpdatePeriodic: 60 
+      EnableDNS: false 
+      DNSType: AsIs 
+      EnableProxyProtocol: false 
+      EnableFallback: false 
+      FallBackConfigs:  
         -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+          SNI: 
+          Path: 
+          Dest: 80 
+          ProxyProtocolVer: 0 
       CertConfig:
-        CertMode: none # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "node1.test.com" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
-        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: test@me.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: hahuutung07.vn@gmail.com
-          CLOUDFLARE_API_KEY: aabb
+        CertMode: file 
+        CertDomain: "admin.4g.tintuc247.top" 
+        CertFile: /etc/XrayR/htpn4g.crt
+        KeyFile: /etc/XrayR/htpn4g.key
+        Provider: cloudflare 
+        Email: lole7176@gmail.
+        DNSEnv: 
+          CLOUDFLARE_EMAIL:
+          CLOUDFLARE_API_KEY:
   -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    PanelType: "V2board" 
     ApiConfig:
-      ApiHost: "$domain"
-      ApiKey: "$APIKEY"
-      NodeID: $node_443
-      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: $SpeedLimit # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: $DeviceLimit # Local settings will replace remote settings, 0 means disable
-      RuleListPath: /etc/XrayR/AikoBlock # /etc/XrayR/rulelist Path to local rulelist file
+      ApiHost: "https://4g.tintuc247.top"
+      ApiKey: "azzphucfreeinternet"
+      NodeID2: 1
+      NodeType: V2ray 
+      Timeout: 30 
+      EnableVless: false 
+      EnableXTLS: false 
+      SpeedLimit: 0
+      DeviceLimit: 0
+      RuleListPath: # /etc/XrayR/rulelist
     ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
+      DisableSniffing: True
+      ListenIP: 0.0.0.0 
+      SendIP: 0.0.0.0 
+      UpdatePeriodic: 60 
+      EnableDNS: false 
+      DNSType: AsIs 
+      EnableProxyProtocol: false 
+      EnableFallback: false 
+      FallBackConfigs:  
         -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+          SNI: 
+          Path: 
+          Dest: 80 
+          ProxyProtocolVer: 0 
       CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "$CertDomain443" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
-        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: test@me.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: hahuutung07.vn@gmail.com
-          CLOUDFLARE_API_KEY: aabb
+        CertMode: file 
+        CertDomain: "4g.tintuc247.top" 
+        CertFile: /etc/XrayR/htpn.crt 
+        KeyFile: /etc/XrayR/htpn.key
+        Provider: cloudflare 
+        Email: lole7176@gmail.com
+        DNSEnv: 
+          CLOUDFLARE_EMAIL: 
+          CLOUDFLARE_API_KEY: 
 EOF
-cd /root
-xrayr restart
+sed -i "s|ApiHost:.*|ApiHost: ${api_host}|" ./config.yml
+sed -i "s|Apikey:.*|Apikey: ${api_key}|" ./config.yml
+sed -i "s|NodeID1:.*|NodeID: ${node_id1}|" ./config.yml
+sed -i "s|NodeID2:.*|NodeID: ${node_id2}|" ./config.yml
+cd /root && xrayr restart && clear && echo -e "   Cài Đặt Hoàn Tất!"
+#Speedtest
+sudo apt install speedtest-cli
+clear
+echo -e "${lam}【KIỂM TRA TỐC ĐỘ MẠNG VPS】${tim}"
+speedtest-cli
